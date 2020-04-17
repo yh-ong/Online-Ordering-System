8464853f8c7494084be0403d7b14f48b1177c840
YH ONG <ongyh97@gmail.com>
2020-04-17 22:44:36 +0800

    Add project files.

--- Online Ordering System/Pages/Cart.cshtml.cs
+++ Online Ordering System/Pages/Cart.cshtml.cs
@@ -1,24 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+
+using System.Data.OleDb;
 using Online_Ordering_System.Model;
 
 namespace Online_Ordering_System.Pages
 {
-    public class IndexModel : PageModel
+    public class CartModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public IEnumerable<Cart> Carts { get; set; }
         public void OnGet()
         {
         }
--- Online Ordering System/Pages/List.cshtml.cs
+++ Online Ordering System/Pages/List.cshtml.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System.Data.SqlClient;
+using Online_Ordering_System.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Online_Ordering_System.Pages
 {
     public class ListModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+
+        public ListModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public IEnumerable<Category> Categories { get; set; }
+        public async Task OnGet()
+        {
+            Categories = await _db.Categories.ToListAsync();
         }
     }
 }
