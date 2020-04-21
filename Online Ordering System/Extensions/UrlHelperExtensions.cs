38ddd9b81b881589430707c398f8b36aa0f2eddb
YH ONG <ongyh97@gmail.com>
2020-04-21 12:37:22 +0800

    add extension for get local url

---
+++ Online Ordering System/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Ordering_System.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string GetLocalUrl(this IUrlHelper urlHelper, string localUrl)
+        {
+            if (!urlHelper.IsLocalUrl(localUrl))
+            {
+                return urlHelper.Page("/Index");
+            }
+
+            return localUrl;
+        }
+    }
+}
