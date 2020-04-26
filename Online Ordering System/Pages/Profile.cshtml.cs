f25a0102b526c44eec15fbd30674df85b06fee4c
YH ONG <ongyh97@gmail.com>
2020-04-26 20:44:33 +0800

    fix: refresh page without changed tab

--- Online Ordering System/Pages/Profile.cshtml.cs
+++ Online Ordering System/Pages/Profile.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Online_Ordering_System.Model;
 
@@ -20,16 +21,74 @@ namespace Online_Ordering_System.Pages
         [BindProperty]
         public User Users { get; set; }
 
-        public async Task OnGetAsync()
+        public int IdentityID()
         {
-            int IdentityID = Int32.Parse(User.Claims.First(c => c.Type.Equals("IdentityID")).Value);
-            Users = await _db.Users.FindAsync(IdentityID);
+            return Int32.Parse(User.Claims.First(c => c.Type.Equals("IdentityID")).Value);
         }
 
-        public void OnPost()
+        public async Task OnGetAsync()
         {
-            int IdentityID = Int32.Parse(User.Claims.First(c => c.Type.Equals("IdentityID")).Value);
-            var dbUser = _db.Users.FirstOrDefault(u => u.UserID.Equals(IdentityID));
+            Users = await _db.Users.FindAsync(IdentityID());
         }
+
+        public async Task<IActionResult> OnPostPersonalAsync()
+        {
+            if(!(String.IsNullOrEmpty(Users.FirstName)) && !(String.IsNullOrEmpty(Users.LastName)))
+            {
+                var dbUser = await _db.Users.FindAsync(IdentityID());
+                dbUser.FirstName = Users.FirstName;
+                dbUser.LastName = Users.LastName;
+                dbUser.PhoneNumber = Users.PhoneNumber;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public async Task<IActionResult> OnPostAccountAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var OldPassword = (from User in _db.Users
+                                  where User.UserID == IdentityID()
+                                  select User.Password).FirstOrDefault();
+                if (OldPassword.Equals(Users.Password) && Users.Password.Equals(Users.Password))
+                {
+                    var dbUser = await _db.Users.FindAsync(IdentityID());
+                    dbUser.Email = Users.Email;
+                    dbUser.Password = Users.Password;
+
+                    await _db.SaveChangesAsync();
+                    return RedirectToPage();
+                }
+                else
+                {
+                    return Page();
+                }
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+
+        public async Task<IActionResult> OnPostAddressAsync()
+        {
+            var dbUser = await _db.Users.FindAsync(IdentityID());
+            dbUser.Address = Users.Address;
+            dbUser.State = Users.State;
+            dbUser.City = Users.City;
+            dbUser.PostalCode = Users.PostalCode;
+            dbUser.Country = Users.Country;
+
+            await _db.SaveChangesAsync();
+            return RedirectToPage();
+        }
+
     }
 }
