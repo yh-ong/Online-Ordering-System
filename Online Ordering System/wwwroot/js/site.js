b6999e5d0c41eb489ad5ea6d4c6f2be858e8ff0a
YH ONG <ongyh97@gmail.com>
2020-05-05 12:19:41 +0800

    style: add responsive layout and animation display

--- Online Ordering System/wwwroot/js/site.js
+++ Online Ordering System/wwwroot/js/site.js
@@ -2,3 +2,45 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(window).scroll(function () {
+    $('.slideanim').each(function () {
+        var pos = $(this).offset().top;
+        var top = $(window).scrollTop();
+        if (pos < top + 600) {
+            $(this).addClass("slide");
+        }
+    });
+
+    var top =  $(window).scrollTop();
+    if (top > 50) {
+        $('#search-navbar').css('top', '-100px');
+        if (top > 100) {
+            $('#search-navbar').addClass("search-navbar-top").removeAttr('style');
+        }
+    } else {
+        $('#search-navbar').removeClass("search-navbar-top").removeAttr('style');
+    }
+
+});
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+});
